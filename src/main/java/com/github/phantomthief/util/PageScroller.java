3b92c467838c53402282d08a5d00397014dda1aa
w.vela <wangtianzhou@kuaishou.com>
2018-12-05 10:22:35 +0800

    [refactor] extract annoy class to inner class.

--- src/main/java/com/github/phantomthief/util/PageScroller.java
+++ src/main/java/com/github/phantomthief/util/PageScroller.java
@@ -1,6 +1,7 @@
 package com.github.phantomthief.util;
 
-import java.util.Collections;
+import static java.util.Collections.emptyList;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
@@ -36,15 +37,6 @@ class PageScroller<Id, Entity> implements Iterable<List<Entity>> {
         this.mode = mode;
     }
 
-    /**
-     * 由于 dao 实现中, start 是被包含的, 使用上一次 cursor 取的时候希望去除 start, 所以还需要多取一个
-     */
-    private static <Id, Entity> List<Entity>
-            fetchOnePageExcludeStart(GetByCursorDAO<Id, Entity> dao, Id start, int limit) {
-        List<Entity> entities = dao.getByCursor(start, limit + 1);
-        return entities.isEmpty() ? entities : entities.subList(1, entities.size());
-    }
-
     public void setMaxNumberOfPages(int maxNumberOfPages) {
         this.maxNumberOfPages = maxNumberOfPages;
     }
@@ -53,67 +45,79 @@ class PageScroller<Id, Entity> implements Iterable<List<Entity>> {
     @Override
     public Iterator<List<Entity>> iterator() {
         if (mode == MODE_START_EXCLUSIVE) {
-            return new AbstractIterator<List<Entity>>() {
-
-                private List<Entity> previousPage;
-                private boolean firstTime = true;
-                private int pageIndex = 0;
-
-                @Override
-                protected List<Entity> computeNext() {
-                    List<Entity> page;
-                    if (firstTime) {
-                        firstTime = false;
-                        // 第一次, 正常取
-                        page = dao.getByCursor(initCursor, bufferSize);
-                    } else {
-                        if (pageIndex >= maxNumberOfPages) {
-                            // 已经取到限制的页数了
-                            page = Collections.emptyList();
-                        } else if (previousPage.size() < bufferSize) {
-                            // 上页还不满, fail fast
-                            page = Collections.emptyList();
-                        } else {
-                            Id start = entityIdFunction
-                                    .apply(previousPage.get(previousPage.size() - 1));
-                            page = fetchOnePageExcludeStart(dao, start, bufferSize);
-                        }
-                    }
-
-                    previousPage = page;
-                    pageIndex++;
-                    return page.isEmpty() ? endOfData() : page;
-                }
-            };
+            return new ExclusiveStartIterator();
         } else {
-            return new AbstractIterator<List<Entity>>() {
-                
-                private int pageIndex = 0;
-                private Id cursor = initCursor;
-                private boolean noNext = false;
+            return new ExclusiveEndIterator();
+        }
+    }
 
-                @Override
-                protected List<Entity> computeNext() {
-                    if (noNext) {
-                        return endOfData();
-                    }
-                    pageIndex++;
-                    if (pageIndex > maxNumberOfPages) {
-                        return endOfData();
-                    }
-                    List<Entity> list = dao.getByCursor(cursor, bufferSize + 1);
-                    if (list.isEmpty()) {
-                        return endOfData();
-                    }
-                    if (list.size() >= bufferSize + 1) {
-                        cursor = entityIdFunction.apply(list.get(bufferSize));
-                        return list.subList(0, bufferSize);
-                    } else {
-                        noNext = true;
-                        return list;
-                    }
+    private class ExclusiveStartIterator extends AbstractIterator<List<Entity>> {
+
+        private List<Entity> previousPage;
+        private boolean firstTime = true;
+        private int pageIndex = 0;
+
+        @Override
+        protected List<Entity> computeNext() {
+            List<Entity> page;
+            if (firstTime) {
+                firstTime = false;
+                // 第一次, 正常取
+                page = dao.getByCursor(initCursor, bufferSize);
+            } else {
+                if (pageIndex >= maxNumberOfPages) {
+                    // 已经取到限制的页数了
+                    page = emptyList();
+                } else if (previousPage.size() < bufferSize) {
+                    // 上页还不满, fail fast
+                    page = emptyList();
+                } else {
+                    Id start = entityIdFunction.apply(previousPage.get(previousPage.size() - 1));
+                    page = fetchOnePageExcludeStart(dao, start, bufferSize);
                 }
-            };
+            }
+
+            previousPage = page;
+            pageIndex++;
+            return page.isEmpty() ? endOfData() : page;
+        }
+
+        /**
+         * 由于 dao 实现中, start 是被包含的, 使用上一次 cursor 取的时候希望去除 start, 所以还需要多取一个
+         */
+        private List<Entity> fetchOnePageExcludeStart(GetByCursorDAO<Id, Entity> dao, Id start,
+                int limit) {
+            List<Entity> entities = dao.getByCursor(start, limit + 1);
+            return entities.isEmpty() ? entities : entities.subList(1, entities.size());
+        }
+    }
+
+    private class ExclusiveEndIterator extends AbstractIterator<List<Entity>> {
+
+        private int pageIndex = 0;
+        private Id cursor = initCursor;
+        private boolean noNext = false;
+
+        @Override
+        protected List<Entity> computeNext() {
+            if (noNext) {
+                return endOfData();
+            }
+            pageIndex++;
+            if (pageIndex > maxNumberOfPages) {
+                return endOfData();
+            }
+            List<Entity> list = dao.getByCursor(cursor, bufferSize + 1);
+            if (list.isEmpty()) {
+                return endOfData();
+            }
+            if (list.size() >= bufferSize + 1) {
+                cursor = entityIdFunction.apply(list.get(bufferSize));
+                return list.subList(0, bufferSize);
+            } else {
+                noNext = true;
+                return list;
+            }
         }
     }
 }
