4d903385f90a634d2989d9dff9a47df6129a5c06
liusixian <liusixian@kuaishou.com>
2020-09-27 20:25:01 +0800

    [javadoc] add javadoc

--- src/main/java/com/github/phantomthief/util/GetByCursorDAO.java
+++ src/main/java/com/github/phantomthief/util/GetByCursorDAO.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.annotation.Nullable;
 
 /**
- * Get data by cursor
- * 
+ * 通过ID作为游标的列表数据访问对象接口
+ *
  * @author w.vela
  */
 public interface GetByCursorDAO<Id, Entity> {
 
     /**
-     * retrieve limited data starting by cursor(inclusive)
+     * 从指定的游标开始（包括），读取limit条记录作为列表返回
+     *
+     * @param cursor 起始游标，包括
+     * @param limit 返回记录数
      */
     List<Entity> getByCursor(@Nullable Id cursor, int limit);
 }
