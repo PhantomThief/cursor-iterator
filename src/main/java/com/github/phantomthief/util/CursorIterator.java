d7234558796379a8865816f367fead5a4702f9bc
Max Liu <starlight36@163.com>
2020-09-27 20:39:51 +0800

    [javadoc] add javadoc (#7)
    Co-authored-by: liusixian

--- src/main/java/com/github/phantomthief/util/CursorIterator.java
+++ src/main/java/com/github/phantomthief/util/CursorIterator.java
@@ -22,6 +22,47 @@ import javax.annotation.Nonnull;
 import com.google.common.collect.AbstractIterator;
 
 /**
+ * 游标迭代器
+ * <p>提供一种迭代器机制，可以连续地遍历瀑布流式的数据接口</p>
+ * <p>例如某个接口允许从指定的ID起始读n条记录返回：</p>
+ * <pre>{@code
+ * interface UserRepository {
+ *     List<User> findAllUsers(Integer startId, int limit);
+ * }
+ * }</pre>
+ * 如果要遍历列表的全部，通常可以设置一个游标ID，并多次调用findAllUsers来完成，例如：
+ * <pre>{@code
+ * void foreachUser() {
+ *     Integer batchSize = 100;
+ *     Integer cursorId = 1;
+ *     while (null != cursorId) {
+ *         List<User> users = findAllUsers(cursorId, batchSize + 1);
+ *         (users.size() > batchSize ? users.subList(0, batchSize) : users).forEach(user -> {
+ *             // 访问单个用户
+ *         });
+ *         if (users.size() >= batchSize + 1) {
+ *            cursorId = users.get(batchSize).getId();
+ *         } else {
+ *            cursorId = null;
+ *         }
+ *     }
+ * }
+ * }</pre>
+ * CursorIterator将这一过程抽象成一个组件，可直接返回一个承载全部列表的迭代器，并在访问时延迟加载后面的分段，使用CursorIterator实现例如：
+ * <pre>{@code
+ * CursorIterator<Integer, User> users = CursorIterator.newGenericBuilder()
+ *         .start(1)
+ *         .bufferSize(100)
+ *         .cursorExtractor(User::getId)
+ *         .buildEx(UserRepository::findAllUsers);
+ * // 直接作为Stream访问
+ * users.stream().forEach(user -> {
+ *     // 访问单个用户
+ * });
+ * }</pre>
+ *
+ * @param <Id> ID类型泛型
+ * @param <Entity> 实体对象泛型
  * @author w.vela
  */
 public class CursorIterator<Id, Entity> implements Iterable<Entity> {
@@ -33,6 +74,13 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
         this.pageScroller = pageScroller;
     }
 
+    /**
+     * 创建通用游标迭代器的构造器
+     *
+     * @param <I> ID泛型类型
+     * @param <E> 返回实体对象的泛型类型
+     * @return 构造器对象
+     */
     @CheckReturnValue
     @Nonnull
     public static <I, E> GenericBuilder<I, E> newGenericBuilder() {
@@ -40,7 +88,7 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
     }
 
     /**
-     * better use {@link #newGenericBuilder()} for type safe
+     * 已废弃：请使用泛型版本{@link #newGenericBuilder()}
      */
     @Deprecated
     @CheckReturnValue
@@ -49,6 +97,11 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
         return new Builder<>();
     }
 
+    /**
+     * 获取迭代器
+     *
+     * @return 返回迭代器对象
+     */
     @Nonnull
     @Override
     public Iterator<Entity> iterator() {
@@ -71,11 +124,22 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
         };
     }
 
+    /**
+     * 获取Stream
+     *
+     * @return 返回一个Stream对象
+     */
     public Stream<Entity> stream() {
         return StreamSupport
                 .stream(spliteratorUnknownSize(iterator(), (NONNULL | IMMUTABLE | ORDERED)), false);
     }
 
+    /**
+     * 泛型游标迭代器构造器
+     *
+     * @param <Id> ID泛型类型
+     * @param <Entity> 实体对象泛型类型
+     */
     public static class GenericBuilder<Id, Entity> {
 
         private final Builder<Object, Object> builder;
@@ -85,7 +149,10 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
         }
 
         /**
-         * 使用 {@link #buildEx} 代替，后者在迭代中删除是友好的
+         * 已废弃：请使用 {@link #buildEx} 代替
+         * <p>警告：此方法在遍历列表删除时有缺陷，会引起跳过删除部分ID，{@link #buildEx}在迭代中删除记录是友好的</p>
+         *
+         * @see #buildEx
          */
         @Deprecated
         @Nonnull
@@ -93,19 +160,37 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
             return builder.build(dao);
         }
 
+        /**
+         * 构造游标迭代器
+         *
+         * @param dao 游标数据访问对象，提供根据初始ID、读取条数读取一列列表的数据访问方法
+         * @return 构造的游标迭代器对象
+         */
         @Nonnull
         public CursorIterator<Id, Entity> buildEx(GetByCursorDAO<? super Id, ? extends Entity> dao) {
             return builder.buildEx(dao);
         }
 
+        /**
+         * 设置游标提取函数
+         *
+         * @param function 提供一个函数，从传入的实体上提取游标ID对象
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public GenericBuilder<Id, Entity>
-                cursorExtractor(Function<? super Entity, ? extends Id> function) {
+        cursorExtractor(Function<? super Entity, ? extends Id> function) {
             builder.cursorExtractor(function);
             return this;
         }
 
+        /**
+         * 设置起始ID，此ID对应的记录将作为迭代器返回的第一条数据对象
+         *
+         * @param init 起始ID
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public GenericBuilder<Id, Entity> start(Id init) {
@@ -113,6 +198,12 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
             return this;
         }
 
+        /**
+         * 设置一次取列表数据返回的记录数
+         *
+         * @param bufferSize 一次取列表数据返回的记录数
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public GenericBuilder<Id, Entity> bufferSize(int bufferSize) {
@@ -120,6 +211,12 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
             return this;
         }
 
+        /**
+         * 设置一次取列表数据返回的记录数的提供器
+         *
+         * @param bufferSize 一次取列表数据返回的记录数提供器函数
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public GenericBuilder<Id, Entity> bufferSize(IntSupplier bufferSize) {
@@ -127,6 +224,12 @@ public class CursorIterator<Id, Entity> implements Iterable<Entity> {
             return this;
         }
 
+        /**
+         * 设置最多取的页数，大于等于1
+         *
+         * @param maxNumberOfPages 最多取的页数
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public GenericBuilder<Id, Entity> maxNumberOfPages(int maxNumberOfPages) {
--- src/main/java/com/github/phantomthief/util/CursorIteratorEx.java
+++ src/main/java/com/github/phantomthief/util/CursorIteratorEx.java
@@ -13,6 +13,34 @@ import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
 /**
+ * 游标迭代器增强版
+ * <p>提供一种迭代器机制，可以连续地遍历瀑布流式的数据接口，相比{@link CursorIterator}，此类允许定制游标处理时的细节</p>
+ * <p>例如某个接口允许从指定的ID起始读n条记录，并返回下一页的起始ID：</p>
+ * <pre>{@code
+ * interface UserRepository {
+ *     ScanResult<User> scan(Integer startId, int limit);
+ * }
+ * class ScanResult {
+ *     private final List<User> users;
+ *     private final Integer nextCursor;
+ *     // getters/setters...
+ * }
+ * }</pre>
+ * 使用CursorIteratorEx可以很简洁地实现遍历过程：
+ * <pre>{@code
+ * Integer startId = 100;
+ * int countPerFetch = 10;
+ * CursorIteratorEx<User, Integer, ScanResult> users = newBuilder()
+ *     .withDataRetriever((Integer cursor) -> repository.scan(cursor, countPerFetch))
+ *     .withCursorExtractor(ScanResult::getNextCursor)
+ *     .withDataExtractor((ScanResult s) -> s.getUsers().iterator())
+ *     .withInitCursor(startId)
+ *     .build();
+ * }</pre>
+ *
+ * @param <T> 返回实体的类型泛型
+ * @param <C> ID类型泛型
+ * @param <R> 列表读取结果对象的泛型
  * @author w.vela
  */
 public class CursorIteratorEx<T, C, R> implements Iterable<T> {
@@ -35,23 +63,45 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
         this.endChecker = endChecker;
     }
 
+    /**
+     * 创建游标迭代器构造器对象
+     *
+     * @return 游标迭代器构造器对象
+     */
     @CheckReturnValue
     @Nonnull
     public static Builder<Object, Object, Object> newBuilder() {
         return new Builder<>();
     }
 
+    /**
+     * 获取迭代器
+     *
+     * @return 返回迭代器对象
+     */
     @Nonnull
     @Override
     public Iterator<T> iterator() {
         return new RollingIterator();
     }
 
+    /**
+     * 获取Stream
+     *
+     * @return 返回一个Stream对象
+     */
     public Stream<T> stream() {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator(),
                 (Spliterator.NONNULL | Spliterator.IMMUTABLE)), false);
     }
 
+    /**
+     * 游标迭代器构造器
+     *
+     * @param <T> 返回实体的类型泛型
+     * @param <C> ID类型泛型
+     * @param <R> 列表读取结果对象的泛型
+     */
     @SuppressWarnings("unchecked")
     public static final class Builder<T, C, R> {
 
@@ -62,6 +112,13 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
         private Function<R, Iterator<T>> dataExtractor;
         private Predicate<C> endChecker;
 
+        /**
+         * 设置起始ID，此ID对应的记录将作为迭代器返回的第一条数据对象
+         *
+         * @param initCursor 起始ID
+         * @param <C1> ID类型泛型
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public <C1> Builder<?, C1, ?> withInitCursor(C1 initCursor) {
@@ -70,6 +127,12 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
             return thisBuilder;
         }
 
+        /**
+         * 设置是否对首个传入的游标进行终末检查
+         *
+         * @param check 是否对首个传入的游标进行终末检查，默认不检查
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public Builder<T, C, R> firstCursorCheckEnd(boolean check) {
@@ -77,6 +140,14 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
             return this;
         }
 
+        /**
+         * 数据读取函数
+         *
+         * @param dataRetriever 数据读取函数，传入当前起始的ID，返回查询结果对象
+         * @param <C1> ID类型泛型
+         * @param <R1> 查询结果泛型
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public <C1, R1> Builder<?, C1, R1> withDataRetriever(Function<C1, R1> dataRetriever) {
@@ -85,6 +156,14 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
             return thisBuilder;
         }
 
+        /**
+         * 获取下一条游标函数
+         *
+         * @param cursorExtractor 获取下一条游标函数，传入当前获得的查询结果对象，返回下一条游标ID对象
+         * @param <C1> ID类型泛型
+         * @param <R1> 查询结果泛型
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public <C1, R1> Builder<?, C1, R1> withCursorExtractor(Function<R1, C1> cursorExtractor) {
@@ -93,15 +172,31 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
             return thisBuilder;
         }
 
+        /**
+         * 数据提取器函数
+         *
+         * @param dataExtractor 数据提取器函数，传入当前获得的查询结果对象，返回结果实体集合的迭代器对象
+         * @param <T1> 实体类型泛型
+         * @param <R1> 查询结果泛型
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public <T1, R1> Builder<T1, ?, R1>
-                withDataExtractor(Function<R1, Iterator<T1>> dataExtractor) {
+        withDataExtractor(Function<R1, Iterator<T1>> dataExtractor) {
             Builder<T1, ?, R1> thisBuilder = (Builder<T1, ?, R1>) this;
             thisBuilder.dataExtractor = dataExtractor;
             return thisBuilder;
         }
 
+        /**
+         * 设置游标终末检查器
+         *
+         * @param endChecker 游标终末检查器，传入当前的游标值，判断是否还有下一条记录存在，有返回true，否则返回false。
+         * 默认根据游标是否为null来进行判断，不为空证明有下一条记录
+         * @param <C1> ID类型泛型
+         * @return 当前构造器对象
+         */
         @CheckReturnValue
         @Nonnull
         public <C1> Builder<?, C1, ?> withEndChecker(Predicate<C1> endChecker) {
@@ -110,6 +205,14 @@ public class CursorIteratorEx<T, C, R> implements Iterable<T> {
             return thisBuilder;
         }
 
+        /**
+         * 构造游标迭代器
+         *
+         * @param <T1> 返回实体的类型泛型
+         * @param <C1> ID类型泛型
+         * @param <R1> 列表读取结果对象的泛型
+         * @return 游标迭代器对象
+         */
         @SuppressWarnings("rawtypes")
         @Nonnull
         public <T1, C1, R1> CursorIteratorEx<T1, C1, R1> build() {
